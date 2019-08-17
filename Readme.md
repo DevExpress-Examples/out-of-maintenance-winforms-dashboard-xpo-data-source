dabfe88601e9ec32d7af72bc9da9f89e3b497eac
BrianDX <boris.zaitsev@devexpress.com>
2019-08-17 13:55:02 +0300

    example content

---
+++ Readme.md
@@ -0,0 +1,10 @@
+<!-- default file list -->
+*Files to look at*:
+* [Form1.cs](./CS/DashboardJsonExample/Form1.cs) (VB: [Form1.vb](./VB/DashboardJsonExample/Form1.vb))
+<!-- default file list end -->
+
+# How to Bind a Dashboard to the XPO Data Source at Runtime
+
+This example creates a dashboard at runtime, binds it to the [DashboardXpoDataSource](https://docs.devexpress.com/Dashboard/DevExpress.DashboardCommon.DashboardXpoDataSource) and displays it in the [DashboardDesigner](https://docs.devexpress.com/Dashboard/DevExpress.DashboardWin.DashboardDesigner) control.
+
+![](/images/screenshot.png)
---
+++ VB/DashboardXpoExample/Form1.vb
@@ -0,0 +1,57 @@
+﻿Imports DevExpress.DashboardCommon
+Imports DevExpress.Xpo
+Imports DevExpress.XtraEditors
+Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+Imports System.Windows.Forms
+
+Namespace DashboardXpoExample
+	Partial Public Class Form1
+		Inherits XtraForm
+
+		Public Sub New()
+			InitializeComponent()
+			dashboardDesigner1.CreateRibbon()
+			InitializeDashboard()
+			dashboardDesigner1.ReloadData()
+		End Sub
+
+		Public Sub InitializeDashboard()
+			Dim dashboard As New Dashboard()
+			Dim xpoDataSource As DashboardXpoDataSource = CreateXpoDataSource()
+
+			dashboard.DataSources.Add(xpoDataSource)
+
+			Dim pivot As New PivotDashboardItem()
+			pivot.DataSource = xpoDataSource
+			pivot.Rows.Add(New Dimension("ContactTitle"))
+			pivot.Columns.Add(New Dimension("Country"))
+			pivot.Values.Add(New Measure("CustomerID", SummaryType.Count))
+
+			Dim chart As New ChartDashboardItem()
+			chart.DataSource = xpoDataSource
+			chart.Arguments.Add(New Dimension("Country"))
+			chart.Panes.Add(New ChartPane())
+			Dim theSeries As New SimpleSeries(SimpleSeriesType.Bar)
+			theSeries.Value = New Measure("CustomerID", SummaryType.Count)
+			chart.Panes(0).Series.Add(theSeries)
+
+			dashboard.Items.AddRange(pivot, chart)
+			dashboard.RebuildLayout()
+			dashboard.LayoutRoot.Orientation = DashboardLayoutGroupOrientation.Vertical
+			dashboardDesigner1.Dashboard = dashboard
+		End Sub
+
+		Public Shared Function CreateXpoDataSource() As DashboardXpoDataSource
+			Dim dataSource As New DashboardXpoDataSource() With {.ConnectionStringName = "northwind"}
+			dataSource.SetEntityType(GetType(nwind.Customers))
+			Return dataSource
+		End Function
+	End Class
+End Namespace
